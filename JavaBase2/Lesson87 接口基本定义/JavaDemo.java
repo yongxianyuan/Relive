6b1650465c662f55b121f26ea47e5fb2e94f2a8b
Yongxian Yuan <yuan.yongxian@icloud.com>
2019-05-05 17:13:38 +0800

    JavaDemo代码优化

--- JavaBase2/Lesson87 接口基本定义/JavaDemo.java
+++ JavaBase2/Lesson87 接口基本定义/JavaDemo.java
@@ -1,7 +1,7 @@
 /*	接口的基本定义
 	
 	抽象类与普通类相比的最大优势在于： 可以实现对子类覆写方法的控制，但是在抽象类里面而普通方法里面可能会涉及
-一些安全或者营私的操作问题，在进行开发的过程中，如果要向外部隐藏全部实现的细节，则就可以通过接口来进行描述。
+一些安全或者隐私的操作问题，在进行开发的过程中，如果要向外部隐藏全部实现的细节，则就可以通过接口来进行描述。
 	
 	接口可以理解为一个纯粹的抽象类，原始定义接口中只包含抽象方法和全局常量。 从JDK1.8开始，由于引入了Lambda表达式
 	的概念， 所以接口的定义也得到了加强，除了抽象方法与全局常量之外，还可以定义普通方法和静态方法。 如果从设计本身角度来讲，接口之中的组成还是应该
@@ -14,15 +14,15 @@
 		}
   
   但是现在很明显的问题出现了，此时的接口肯定无法直接产生实例化对象,所以对于接口
-  	×接口需要被子类实现(implements) ,一个子类可以实现多个接口
-  	×子类（如果不是抽象类） 那么一定要覆写接口之中的全部抽象方法
-	×接口对象可以利用子类对象的向上转型进行实例化
+  	*接口需要被子类实现(implements) ,一个子类可以实现多个接口
+  	*子类（如果不是抽象类） 那么一定要覆写接口之中的全部抽象方法
+	*接口对象可以利用子类对象的向上转型进行实例化
 
 
 	interface IMessage {
 	public static final String INFO = "www.github.com" ; //全局常量
 	public abstract String getInfo() ;		//抽象方法
-}
+	}
 接口实现用的是implements关键字
 class MessageImpl implements IMessage{
 	public String getInfo(){
@@ -67,7 +67,7 @@ public class JavaDemo {
 
 	Object 类对象可以接受所有数据类型，包括基本数据类型、类对象、接口对象、数组。
 
-	由于接口描述的一个公共定义标准，所有抽象方法访问权限都是public的 。
+	由于接口描述的一个公共定义标准，所有抽象方法访问权限都是public的,接口中关键字public可以省略 。
 */
 
 interface IMessage {
@@ -92,9 +92,9 @@ class MessageImpl implements IMessage, IChannel{
 public class JavaDemo {
 	public static void main(String []args) {
 		IMessage msg = new MessageImpl() ;
-		Object obj = msg;
+		Object obj = msg;   //接口也是一个Object实例对象
 		// IChannel chl = (IChannel)msg;
-		IChannel chl = (IChannel)obj;
+		IChannel chl = (IChannel)obj;  //强制类型转换
 		// System.out.println(chl.getInfo());
 		// System.out.println(chl.getInfo());
 		
